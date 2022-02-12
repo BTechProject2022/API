709230efe9f6ecffee87262625669f7657162f08
jainpratik830 <jainpratik830@gmail.com>
2022-02-12 10:06:37 +0530

    Added KeyPair API Endpoint

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { didContract, web3 } = require('./abi');
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const secp256k1 = require("ethereum-cryptography/secp256k1");
+const secp =  require("@noble/secp256k1");
+const { bytesToHex } = require("ethereum-cryptography/utils");
 
 const PUBLIC_ADDRESS = process.env.PUBLIC_ADDRESS;
 
@@ -23,6 +27,24 @@ app.get('/getDIDDoc/:did', async (req, res) => {
 			publicKey: didDoc[2].publicKey
 		}
 		res.status(200).json({ context: didDoc[0], did: didDoc[1], key: key });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: err });
+	}
+});
+
+app.get('/keyPair', async (req, res) => {
+	// const did = decodeURI(req.params.did);
+	console.log("here");
+	const privateKeyBytes = secp.utils.randomPrivateKey();
+	const privateKey = secp.utils.bytesToHex(privateKeyBytes)
+	const publicKeyBytes = secp.getPublicKey(privateKeyBytes);
+	const publicKey = secp.utils.bytesToHex(publicKeyBytes)
+	const add = bytesToHex(keccak256((publicKeyBytes)));
+	const address = '0x'+add.substring(24);
+
+	try {
+		res.status(200).json({ 'PrivateKey': privateKey, 'PublicKey':publicKey, "Address":address });
 	} catch (err) {
 		console.log(err);
 		res.status(500).json({ error: err });
